9f1ab9bffac87fc7e4744d212e294ff6639581dd
Denis Piyalkin <pialkind@mail.ru>
2022-12-20 18:47:57 +0300

    + MyPow()

--- funcs.cpp
+++ funcs.cpp
@@ -12,3 +12,16 @@ double MyMul(double a, double b)
 {
 return a * b;
 }
+double MyDiv(double a, double b)
+{
+return a / b;
+}
+int MyPow(int num, int n)
+{
+int res = num;
+for (int i = 1; i < n; i++)
+{
+res *= num;
+}
+return res;
+}
